d710b07847309d8cedefc34897ff3505121940c6
Meabeab <malikovigor2004@mail.ru>
2024-10-06 21:59:58 +0400

    commit

--- pages/main.js
+++ pages/main.js
@@ -1,3 +1,24 @@
+// const ↓
+const tabs = document.querySelectorAll(".models__button");
+const contents = document.querySelectorAll(".models__list");
+
+// function ↓
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("models__button-active");
+    });
+    contents.forEach((content) => {
+      content.classList.remove("models__list-active");
+    });
+
+    tabs[index].classList.add("models__button-active");
+    contents[index].classList.add("models__list-active");
+    
+  });
+});
+
 initialIPhones15.forEach(function (item) {
   renderModels15(item);
 });
