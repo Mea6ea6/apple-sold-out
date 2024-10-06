81da42e460acaf97c5d2730ca2ea4d7b962930b4
Meabeab <malikovigor2004@mail.ru>
2024-10-07 00:48:56 +0400

    commit

--- pages/main.js
+++ pages/main.js
@@ -1,24 +1,22 @@
-// const ↓
+// Tabs
 const tabs = document.querySelectorAll(".models__button");
 const contents = document.querySelectorAll(".models__list");
 
-// function ↓
 tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
-
     tabs.forEach((tab) => {
       tab.classList.remove("models__button-active");
     });
     contents.forEach((content) => {
       content.classList.remove("models__list-active");
     });
-
     tabs[index].classList.add("models__button-active");
     contents[index].classList.add("models__list-active");
     
   });
 });
 
+// Models
 initialIPhones15.forEach(function (item) {
   renderModels15(item);
 });
@@ -36,4 +34,30 @@ initialIPhones11.forEach(function (item) {
 });
 initialIPhones10.forEach(function (item) {
   renderModels10(item);
+});
+
+// Slider
+const sliderWrapper = document.querySelector(".faq__items");
+const slides = document.querySelectorAll(".faq__item");
+const leftArrow = document.querySelector(".faq__button-left");
+const rightArrow = document.querySelector(".faq__button-right");
+
+let currentSlide = 0;
+const totalSlides = slides.length;
+
+function updateSliderPosition() {
+  const newTranslateValue = -(390 * currentSlide);
+  sliderWrapper.style.transform = `translateX(${newTranslateValue}px)`;
+}
+leftArrow.addEventListener("click", () => {
+  if (currentSlide > 0) {
+    currentSlide--;
+    updateSliderPosition();
+  }
+});
+rightArrow.addEventListener("click", () => {
+  if (currentSlide < totalSlides - 3) {
+    currentSlide++;
+    updateSliderPosition();
+  }
 });
