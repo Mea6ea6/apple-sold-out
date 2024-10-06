843c885865c955837c2f49c683b72e7f76597400
Meabeab <malikovigor2004@mail.ru>
2024-10-06 20:01:21 +0400

    commit

--- pages/main.js
+++ pages/main.js
@@ -1,18 +1,18 @@
-const tabs = document.querySelectorAll(".models__button");
-const contents = document.querySelectorAll(".models__list");
-
-tabs.forEach((tab, index) => {
-  tab.addEventListener("click", () => {
-
-    tabs.forEach((tab) => {
-      tab.classList.remove("models__button-active");
-    });
-    contents.forEach((content) => {
-      content.classList.remove("models__list-active");
-    });
-
-    tabs[index].classList.add("models__button-active");
-    contents[index].classList.add("models__list-active");
-    
-  });
+initialIPhones15.forEach(function (item) {
+  renderModels15(item);
+});
+initialIPhones14.forEach(function (item) {
+  renderModels14(item);
+});
+initialIPhones13.forEach(function (item) {
+  renderModels13(item);
+});
+initialIPhones12.forEach(function (item) {
+  renderModels12(item);
+});
+initialIPhones11.forEach(function (item) {
+  renderModels11(item);
+});
+initialIPhones10.forEach(function (item) {
+  renderModels10(item);
 });
---
+++ utils/constants.js
@@ -0,0 +1,224 @@
+const initialIPhones15 = [
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Базовый",
+    memory_first: "128 Гб —",
+    price_first: "45.000~50.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "50.000~60.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Plus",
+    memory_first: "128 Гб —",
+    price_first: "45.000~55.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "55.000~65.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro",
+    memory_first: "128 Гб —",
+    price_first: "65.000~75.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "70.000~80.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro Max",
+    memory_first: "256 Гб —",
+    price_first: "75.000~85.000 ₽",
+    memory_second: "512 Гб —",
+    price_second: "80.000~90.000 ₽",
+  }
+];
+
+const initialIPhones14 = [
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Базовый",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: ["#494a4c", "red", "green"],
+    title: "Plus",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro Max",
+    memory_first: "256 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "512 Гб —",
+    price_second: "00.000~00.000 ₽",
+  }
+];
+
+const initialIPhones13 = [
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "mini",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Базовый",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro Max",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  }
+];
+
+const initialIPhones12 = [
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "mini",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "128 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Базовый",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "128 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro Max",
+    memory_first: "128 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  }
+];
+
+const initialIPhones11 = [
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Базовый",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "128 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Pro Max",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  }
+];
+
+const initialIPhones10 = [
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "Базовый",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "XR",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "128 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "XS Max",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  },
+  {
+    image: "./images/models/__iphone.png",
+    colors: "#494a4c",
+    title: "XS",
+    memory_first: "64 Гб —",
+    price_first: "00.000~00.000 ₽",
+    memory_second: "256 Гб —",
+    price_second: "00.000~00.000 ₽",
+  }
+];
