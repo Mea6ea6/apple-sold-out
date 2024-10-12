ed582021f0427951530b795456e8f7461f5ed62e
Meabeab <malikovigor2004@mail.ru>
2024-10-12 19:03:11 +0400

    commit

--- pages/main.js
+++ pages/main.js
@@ -41,14 +41,28 @@ const sliderWrapper = document.querySelector(".faq__items");
 const slides = document.querySelectorAll(".faq__item");
 const leftArrow = document.querySelector(".faq__button-left");
 const rightArrow = document.querySelector(".faq__button-right");
-
-let currentSlide = 0;
 const totalSlides = slides.length;
+let currentSlide = 0;
 
+function getMaxVisibleSlides() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 1180) return 3;
+  else if (screenWidth > 780) return 2;
+  else return 1;
+}
 function updateSliderPosition() {
   const newTranslateValue = -(390 * currentSlide);
   sliderWrapper.style.transform = `translateX(${newTranslateValue}px)`;
+  updateArrows();
 }
+function updateArrows() {
+  leftArrow.classList.toggle("faq__button_disabled", currentSlide === 0);
+  rightArrow.classList.toggle(
+    "faq__button_disabled",
+    currentSlide >= totalSlides - getMaxVisibleSlides()
+  );
+}
+
 leftArrow.addEventListener("click", () => {
   if (currentSlide > 0) {
     currentSlide--;
@@ -56,8 +70,11 @@ leftArrow.addEventListener("click", () => {
   }
 });
 rightArrow.addEventListener("click", () => {
-  if (currentSlide < totalSlides - 3) {
+  if (currentSlide < totalSlides - getMaxVisibleSlides()) {
     currentSlide++;
     updateSliderPosition();
   }
 });
+
+window.addEventListener("resize", updateArrows);
+updateArrows();
